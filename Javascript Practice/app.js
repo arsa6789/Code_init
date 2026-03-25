813a11db655cf6ffdec28569f24bc913cd9234a0
arsa6789 <sajedaparveenshafi@gmail.com>
2026-03-25 17:43:18 +0500

    Made CSS cool button and Javascript Practice folder

---
+++ Javascript Practice/app.js
@@ -0,0 +1,73 @@
+// Alert Excercise
+// alert("Welcome user");
+// alert("Error! Please enter a valid password.");
+// alert("Welcome to JS land... \n" + "Happy coding!");
+// alert("Welcome to JS land..");
+// alert("Happy coding!! \n" + "Prevent this page from creating additional dialogs");
+
+// Variable for Strings
+// let username = "Hello";
+// let myName = "Abdul Rehman";
+// let message = "Hello World";
+// alert(message)
+// Student Data Store
+// let usrname = "Jhons Coe";
+// let age = 5 + " " + "years old";
+// let course = "Certified Mobile Application Development";
+// alert(usrname);
+// alert(age);
+// alert(course);
+// let pizza = "PIZZA \n" + "PIZZ \n" + "PIZ \n" + "PI \n" + "P";
+// alert(pizza)
+// let email = "My" + " " + "email address" + " " + "is" + " " + "arsa014444@gmail.com";
+// alert(email)
+// let book = "I am trying to learn from a book called \n" + "A Smarter way to Learn Javascript";
+// alert(book);
+// let code = "YAY! I can write HTML content through Javascript";
+// document.write(code)
+// alert("▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬")
+
+// Variable for numbers
+// const age = "I am"+ " " + 15 + " "+ "years old";
+// alert(age);
+// const usrname = prompt("Enter your name");
+// const prod = prompt("Product name you want to buy?? \n" + "shirts") ;
+// const quantity = prompt("How much you want to buy?? \n" + "1, 2, 3, 4 or 100");
+// document.write("Your name is:" + usrname + "  " + "Your Product:" + " " + prod + " "+ "Your buyed:"+ " " + quantity + "shirt(s)")
+
+//Variable names : Legal && Illegal
+// let x,y,z;
+//legal
+// var myNumber = 6;
+// var _myLike = 8;
+// var lo7 = 67;
+// let $mySit = "fork";
+//illegal
+// var my user = "user";
+// var @statue = "portugal";
+// var -sio = "sick";
+// var let = 22;
+// var function = "lolipop";
+// document.write("<h1>Rules for assigning variable names</h1>");
+// document.write("<br>");
+// document.write("Variable name can only conatin" + " "+ "numbers" + " " + ", $" + " " + "and" + " " + "_" + ".");
+// document.write("For example:" + " " + "$my_1stVaribale");
+// document.write("<br>");
+// document.write("Variable must begin with a" + " "+ "letter" + " " + ", $" + " " + "and" + " " + "_" + ".");
+// document.write("For example:" + " " + "$name" + " " + ", _name" + " "+ "and" + " "+ "name");
+// document.write("<br>");
+// document.write("Variable names are case" + " " +"sensitive");
+// document.write("<br>");
+// document.write("Variables should not be JS" + "<b> Keywords</b>");
+
+// Math expressions
+// const num1 = +prompt("Enter 1st number:");
+// const num2 = +prompt("Enter 2nd number");
+// document.write("Sum of" + " " + num1 + " "+ "and" +" " + num2 + " " + "is ");
+// document.write(num1 + num2 + "<br>");
+// document.write("Product of" + " " + num1 + " "+ "and" +" " + num2 + " " + "is ");
+// document.write(num1 * num2 + "<br>");
+// document.write("Difference of" + " " + num1 + " "+ "and" +" " + num2 + " " + "is ");
+// document.write(num1 - num2 + "<br>");
+// document.write("Quotient of" + " " + num1 + " "+ "and" +" " + num2 + " " + "is ");
+// document.write(num1 / num2);
